856373965e285177b106537adca0ce9edfb4eb66
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2024-01-09 22:07:30 -0500

    Updated projects page

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,9 +1,11 @@
 import "./ProjectsPage.scss";
 import { motion } from "framer-motion";
 import ButtonPills from "../../components/ButtonPills/ButtonPills";
+import { HiLink } from "react-icons/hi2";
+import weatherImg from "../../assets/images/weather-app.png";
+import portalDemoLoginImg from "../../assets/images/portal-demo-login.png";
 
 const ProjectsPage = () => {
-
   return (
     <motion.div
       className="projectspage-container"
@@ -13,56 +15,98 @@ const ProjectsPage = () => {
     >
       <h2>Projects</h2>
 
-      <div className="row">
-        <div>
-          <a
-            href="https://github.com/Brijsunil2/portal"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h4>Portal</h4>
-          </a>
-          <h5>Frontend / Backend</h5>
-          <p>
-            This project is my current on going project so far. The goal of this
-            project is to make a website domain that hosts a few apps.
-            <br /> <br />
-            The homepage contains login / signup, time and weather sections.
-            Weather data is fetched from OpenWeatherMap API. Once user is
-            logged-in the user will have access to a users forum page where they
-            can create user forums similar to how Reddit works.
-          </p>
-          <div className="pills-list d-flex flex-wrap">
-            <ButtonPills name="React" />
-            <ButtonPills name="MongoDB" />
-            <ButtonPills name="JavaScript" />
-            <ButtonPills name="CSS" />
-            <ButtonPills name="HTML" />
-            <ButtonPills name="Bootstrap" />
+      <div className="my-3">
+        <div className="row m-1">
+          <div className="col-2">
+          <img src={portalDemoLoginImg} alt="Portal demo login page" />
+          </div>
+          <div className="col">
+            <h4>Portal-Demo</h4>
+            <h5>Frontend / Backend</h5>
+          </div>
+        </div>
+        <div className="row m-1">
+          <div className="col-2">
+            <a
+              href="https://github.com/Brijsunil2/portal"
+              target="_blank"
+              rel="noreferrer"
+              className="d-flex"
+            >
+              <HiLink className="m-1" /> Github Repo
+            </a>
+            <a
+              href="https://portal-demo.onrender.com"
+              target="_blank"
+              rel="noreferrer"
+              className="d-flex"
+            >
+              <HiLink className="m-1" /> Visit Site
+            </a>
+          </div>
+          <div className="col">
+            <p>
+              A website used to act as a template user authentication site. The
+              user is able to create an account, login and view a varity of web
+              based applications. Since this is the demo verson the sites are
+              just place holders. The focus here was user authentication.
+            </p>
+            <div className="pills-list d-flex flex-wrap">
+              <ButtonPills name="React" />
+              <ButtonPills name="Vite" />
+              <ButtonPills name="MongoDB" />
+              <ButtonPills name="JavaScript" />
+              <ButtonPills name="CSS" />
+              <ButtonPills name="HTML" />
+              <ButtonPills name="Bootstrap" />
+              <ButtonPills name="React Bootstrap" />
+              <ButtonPills name="Redux Toolkit" />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="row">
-        <div>
-          <a
-            href="https://github.com/Brijsunil2/weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
+      <div className="my-3">
+        <div className="row m-1">
+          <div className="col-2">
+            <img src={weatherImg} alt="Weather app homepage" />
+          </div>
+          <div className="col">
             <h4>Weather App</h4>
-          </a>
-          <h5>Frontend</h5>
-          <p>
-            This web app utilizes the OpenWeatherMap API to fetch weather
-            conditions for the current user location or a searched user
-            location. Used Webpack as the bundler and vanilla JavaScript.
-          </p>
-          <div className="pills-list d-flex flex-wrap">
-            <ButtonPills name="JavaScript" />
-            <ButtonPills name="CSS" />
-            <ButtonPills name="HTML" />
-            <ButtonPills name="Webpack" />
+            <h5>Frontend</h5>
+          </div>
+        </div>
+        <div className="row m-1">
+          <div className="col-2">
+            <a
+              href="https://github.com/Brijsunil2/weather-app"
+              target="_blank"
+              rel="noreferrer"
+              className="d-flex align-items-center"
+            >
+              <HiLink className="m-1" /> Github Repo
+            </a>
+            <a
+              href="https://weather-app-ffud.onrender.com"
+              target="_blank"
+              rel="noreferrer"
+              className="d-flex align-items-center"
+            >
+              <HiLink className="m-1" /> Visit Site
+            </a>
+          </div>
+          <div className="col">
+            <p>
+              This web app utilizes the OpenWeatherMap API to fetch weather
+              conditions for the current user location or a searched user
+              location. Used Webpack as the bundler and vanilla JavaScript.
+            </p>
+            <div className="pills-list d-flex flex-wrap">
+              <ButtonPills name="JavaScript" />
+              <ButtonPills name="CSS" />
+              <ButtonPills name="HTML" />
+              <ButtonPills name="Webpack" />
+            </div>
           </div>
         </div>
       </div>
