3508b2568b11150d54cb26529da0842939c2a17d
Sunil Brijlall <brijsunil2@gmail.com>
2025-02-09 18:42:57 -0500

    removed routes redirects for all pages except homepage for one-page layout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.scss";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
-import ContactPage from "./pages/ContactPage/ContactPage";
 
 function App() {
   return (
@@ -12,9 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="contact" element={<ContactPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
