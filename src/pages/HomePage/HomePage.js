3ece37f3a690098314ccd56512f9d564e4b1c0f1
Sunil Brijlall <brijsunil2@gmail.com>
2025-02-09 17:48:05 -0500

    refactor AboutPage to AboutSection for one-page portfolio design

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import "./HomePage.scss";
 import IntroSection from "../../sections/IntroSection/IntroSection";
+import AboutSection from "../../sections/AboutSection/AboutSection";
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <IntroSection />
+      <AboutSection />
     </div>
   );
 };
