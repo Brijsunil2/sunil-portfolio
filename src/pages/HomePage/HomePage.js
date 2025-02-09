7c8b760c158735dfcc6a5d61826a146c60eab13a
Sunil Brijlall <brijsunil2@gmail.com>
2025-02-09 16:49:38 -0500

    refactor home page content to IntroSection component for better modularity

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,73 +1,12 @@
 import "./HomePage.scss";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
-import Picture from "../../assets/images/guy_on_computer.svg";
 import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import IntroSection from "../../sections/IntroSection/IntroSection";
 
 const HomePage = () => {
   return (
-    <motion.div
-      className="homepage-container d-flex flex-column align-items-center"
-      initial={{ x: 1000 }}
-      animate={{ x: 0 }}
-      transition={{ duration: 0.4 }}
-    >
-      <h2>
-        Hi, I'm <span>Sunil Brijlall</span>
-      </h2>
-
-      <h4>Computer Science Graduate | Full-Stack Developer</h4>
-      <p>
-      Motivated Computer Science graduate with a solid foundation in
-          programming and a deep passion for full-stack development. I am eager
-          to apply my academic knowledge, practical skills, and relevant work
-          experience to contribute effectively to innovative and challenging
-          projects. My expertise spans HTML, CSS, JavaScript, and React, with a
-          focus on creating responsive and user-friendly applications. Explore
-          my portfolio to see how I bring ideas to life and drive projects to
-          success.
-      </p>
-
-      <motion.div
-        transition={{ duration: 0.1 }}
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        <NavLink className="contact-link" to="/contact" exact="true">
-          <FaEnvelope />
-          Contact Me
-        </NavLink>
-      </motion.div>
-
-      <div className="socials-container">
-        <motion.div
-          transition={{ duration: 0.1 }}
-          whileHover={{ scale: 1.2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <a
-            href="https://www.linkedin.com/in/sunil-brijlall/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedin />
-          </a>
-        </motion.div>
-        <motion.div
-          transition={{ duration: 0.1 }}
-          whileHover={{ scale: 1.2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <a
-            href="https://github.com/Brijsunil2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub />
-          </a>
-        </motion.div>
-      </div>
-    </motion.div>
+   <IntroSection />
   );
 };
 
