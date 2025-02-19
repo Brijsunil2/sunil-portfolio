867f875981e7a84227457fb132b719bcbf0fb9cc
Sunil Brijlall <brijsunil2@gmail.com>
2025-02-18 19:52:08 -0500

    fix: add keys to experience section articles and list items

--- src/sections/ExperienceSection/ExperienceSection.jsx
+++ src/sections/ExperienceSection/ExperienceSection.jsx
@@ -5,22 +5,22 @@ const ExperienceSection = () => {
   return (
     <section id="experiences" className="experiences-section">
       <h2>Experience</h2>
-      {experienceData.map((experience) => (
-        <article className="experience-wrapper">
+      {experienceData.map((experience, i) => (
+        <article key={i} className="experience-wrapper">
           <h3>{experience.company}</h3>
           <p className="duration-label">{experience.duration}</p>
           {
-            experience.positions.map((position) => (
-              <>
+            experience.positions.map((position, j) => (
+              <div key={j}>
                 <h4>{position.jobTitle}</h4>
                 <ul>
                 {
-                  position.responsibilities.map((responsibility) => (
-                    <li>{responsibility}</li>
+                  position.responsibilities.map((responsibility, k) => (
+                    <li key={k}>{responsibility}</li>
                   ))
                 }
                 </ul>
-              </>
+              </div>
             ))
           }
         </article>
