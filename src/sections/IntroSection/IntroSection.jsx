8f3b437a3e80b1e71066265bbe5a3e4dd1893d2c
Sunil Brijlall <brijsunil2@gmail.com>
2025-02-19 18:20:37 -0500

    styles: add basic styles to intro section for desktop screens

--- src/sections/IntroSection/IntroSection.jsx
+++ src/sections/IntroSection/IntroSection.jsx
@@ -3,30 +3,33 @@ import { introData } from "../../portfolioData";
 
 const IntroSection = () => {
   return (
-    <section className="intro-section">
-      <h1>
-        Hi, I'm <span>{introData.name}</span>
-      </h1>
-      <h3>
-        {introData.qualifications.map((qualification, index) => (
-          <span key={index}>
-            {qualification}
-            {index < introData.qualifications.length - 1 && " | "}
-          </span>
-        ))}
-      </h3>
-      <p>{introData.intro}</p>
+    <section className="intro-section wrapper-width-md">
+      <div className="intro-section-wrapper">
+        <h1>
+          Hi, I'm <span>{introData.name}.</span>
+        </h1>
+        <h5>
+          {introData.qualifications.map((qualification, index) => (
+            <span key={index}>
+              {qualification}
+              {index < introData.qualifications.length - 1 && " | "}
+            </span>
+          ))}
+        </h5>
+        <p>{introData.intro}</p>
 
-      <div className="socials-container">
-        {introData.links.map((link, index) => (
-          <div
-            key={index}
-          >
-            <a href={link.href} target={link.target ? "_blank" : ""} rel="noreferrer">
-              <link.Icon />
-            </a>
-          </div>
-        ))}
+        <div className="socials-container">
+          {introData.links.map((link, index) => (
+              <a
+                key={index}
+                href={link.href}
+                target={link.target ? "_blank" : ""}
+                rel="noreferrer"
+              >
+                <link.Icon />
+              </a>
+          ))}
+        </div>
       </div>
     </section>
   );
