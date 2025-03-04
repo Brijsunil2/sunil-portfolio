a40191ec96d0ed260f53d46743e5e2886a6d3353
Sunil Brijlall <brijsunil2@gmail.com>
2025-03-03 20:45:07 -0500

    style: add openModal animation for polish

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
+import "./styles/animations.scss";
 import App from "./App";
+
+// window.addEventListener("scroll", () => {
+//   var element = document.querySelector(".scroll-fade-in");
+//   var position = element.getBoundingClientRect();
+
+//   if (position.top < window.innerHeight && position.bottom >= 0) {
+//     element.classList.add("active");
+//   }
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
